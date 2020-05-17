a654c05287e491b1a28de6aee748c38d83e8db2a
polojerry <polojerry4@gmail.com>
2020-05-17 22:44:48 +0300

    Used FloatLayout for dynacmic placing of widgets

---
+++ Lesson5.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+
+
+class HomeApp(App):
+    def build(self):
+        return FloatLayout()
+
+
+if __name__ == "__main__":
+    HomeApp().run()
